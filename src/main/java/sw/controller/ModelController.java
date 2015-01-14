35eb255f7958aead54411a9f78e0ffc8c24eb67b
Carolina Dávalos <karo9211.7@gmail.com>
2015-01-14 08:23:29 +0000

    .

--- src/main/java/sw/controller/ModelController.java
+++ src/main/java/sw/controller/ModelController.java
@@ -4,6 +4,9 @@ package sw.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
 
 @Controller
 @RequestMapping(value="/modelo")
@@ -11,4 +14,10 @@ public class ModelController
 {
   @Autowired
     ModeloServices soamodelo;
+  
+  @RequestMapping(value = "/todos", method=RequestMethod.GET)
+    public @ResponseBody List<Modelo> getAll()
+    {
+        return soapuntoi.obtenerTodos();
+    }
 }
