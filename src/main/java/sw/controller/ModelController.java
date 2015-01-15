df568055e61f411d6c317ea1be07fc08268ee857
disiembre24@hotmail.com <disiembre24@hotmail.com>
2015-01-15 08:05:29 +0000

    a

--- src/main/java/sw/controller/ModelController.java
+++ src/main/java/sw/controller/ModelController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import sw.model.Modelo;
 import sw.services.ModeloServices;
@@ -23,9 +24,17 @@ public class ModelController
     {
 		 return soamodelo.obtenerTodos();
     }
- /* @RequestMapping(value ="/modelo/{id}")
+  
+  @RequestMapping(value ="/modelo/{id}")
   @ResponseBody
-  public Modelo obtenerUno( @PathVariable("id") int id){
+  public Modelo obtenerUno( @PathVariable("id") int id)
+  {
     return soamodelo.obtenerUno(id);
-  }*/
+  }
+  @RequestMapping(value ="/modelo/nombre/{nombre}")
+  @ResponseBody
+  public Modelo obtenerXNombre( @PathVariable("nombre") String nombre)
+  {
+    return soamodelo.obtenerXNombre(nombre);
+  }
 }
